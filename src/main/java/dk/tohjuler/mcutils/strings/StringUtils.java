d70ca53f56efc47406d10f4ad8f4893d0677438f
Tohjuler <tobias@tohjuler.dk>
2024-10-21 17:26:31 +0200

    feat: StringUtils

---
+++ src/main/java/dk/tohjuler/mcutils/strings/StringUtils.java
@@ -0,0 +1,34 @@
+package dk.tohjuler.mcutils.strings;
+
+/**
+ * A utility class for working with strings.
+ */
+public class StringUtils {
+
+    /**
+     * Generates a random string of the given length.
+     * Characters are picked from the set [A-Z][a-z][0-9].
+     * <br>
+     * @param length The length of the string to generate.
+     * @return A random string of the given length.
+     */
+    public static String randomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            result.append(characters.charAt((int) (Math.random() * characters.length())));
+        }
+        return result.toString();
+    }
+
+    /**
+     * Repeats a string a given number of times.
+     * <br>
+     * @param str The string to repeat.
+     * @param times The number of times to repeat the string.
+     * @return The repeated string.
+     */
+    public static String repeat(String str, int times) {
+        return new String(new char[times]).replace("\0", str);
+    }
+}
