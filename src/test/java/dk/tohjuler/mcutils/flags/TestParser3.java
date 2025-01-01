9466de3c7554f8d5a76537300e77c763958c3c2f
Tohjuler <tobias@tohjuler.dk>
2025-01-02 00:10:13 +0100

    fix: ValueFlag

---
+++ src/test/java/dk/tohjuler/mcutils/flags/TestParser3.java
@@ -0,0 +1,27 @@
+package dk.tohjuler.mcutils.flags;
+
+import static org.junit.Assert.*;
+
+public class TestParser3 extends FlagParser {
+    private final Flag testFlag = new Flag(
+            "test",
+            "t",
+            "Used to test"
+    );
+
+    private final ValueFlag<String> testFlag2 = new ValueFlag<>(
+            "valuetest",
+            "v",
+            "Used to test2"
+    );
+
+    private final HelpFlag helpFlag = new HelpFlag();
+
+    @Override
+    public void onParsed(String parsed) {
+        assertEquals("", parsed);
+        assertFalse(testFlag.isEnabled());
+        assertTrue(helpFlag.isEnabled());
+        assertEquals("HERE", testFlag2.getValue());
+    }
+}
